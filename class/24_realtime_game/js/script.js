c4b2c10a6ebf7479914f96f50e7ff61c772bdf6e
Sathras <web@fuchsberger.us>
2017-03-30 17:44:59 +0000

    updated grades

---
+++ class/24_realtime_game/js/script.js
@@ -0,0 +1,12 @@
+$('div').click(function (){
+  $(this).addClass('black');
+});
+
+$('div[data-filter="1"]').click(function (){
+  $(this).addClass('black');
+});
+
+
+
+$().data('key');
+$().data('key', 'value')
