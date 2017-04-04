f8f7fb8f4bfa6cd572c3fa746c951e2278e46e0c
Sathras <web@fuchsberger.us>
2017-04-04 16:25:08 +0000

    updated prog25

--- class/24_realtime_game/js/game.js
+++ class/24_realtime_game/js/game.js
@@ -9,126 +9,108 @@ document.body.appendChild(canvas);
 var bgReady = false;
 var bgImage = new Image();
 bgImage.onload = function () {
-	bgReady = true;
+  bgReady = true;
 };
-bgImage.src = "2.png";
+bgImage.src = "img/bg.png";
 
-
+// Hero Image
 var heroReady = false;
 var heroImage = new Image();
 heroImage.onload = function () {
-	heroReady = true;
+  heroReady = true;
 };
-heroImage.src = "1.png";
+heroImage.src = "img/hero1.png";
 
-var monsterReady = false;
-var monsterImage = new Image();
-monsterImage.onload = function () {
-	monsterReady = true;
+// Treasure Image
+var treasureReady = false;
+var treasureImage = new Image();
+treasureImage.onload = function () {
+  treasureReady = true;
 };
-monsterImage.src = "http://thumb.vpgamecdn.com/file/npc_dota_hero_dragon_knight_sm.png";
+treasureImage.src = "img/gold.png";
 
 // Game objects
 var hero = {
-	speed: 256, // movement in pixels per second
-	x: 0,
-	y: 0
+  speed: 256, // movement in pixels per second
+  x: 0,
+  y: 0
 };
-var monster = {
-	x: 0,
-	y: 0
+var treasure = {
+  x: 0,
+  y: 0
 };
-var monstersCaught = 0;
-
+var treasuresCaught = 0;
 
 // Handle keyboard controls
 var keysDown = {};
+$(window).keydown(function(e){ keysDown[e.keyCode] = true; });
+$(window).keyup  (function(e){ delete keysDown[e.keyCode]; });
 
-addEventListener("keydown", function (e) {
-	keysDown[e.keyCode] = true;
-}, false);
-
-addEventListener("keyup", function (e) {
-	delete keysDown[e.keyCode];
-}, false);
-
-
-// Reset the game when the player catches a monster
+// Reset the game when the player catches a treasure
 var reset = function () {
-	hero.x = canvas.width / 2;
-	hero.y = canvas.height / 2;
+  hero.x = canvas.width / 2;
+  hero.y = canvas.height / 2;
 
-	// Throw the monster somewhere on the screen randomly
-	monster.x = 32 + (Math.random() * (canvas.width - 64));
-	monster.y = 32 + (Math.random() * (canvas.height - 64));
+  // Throw the treasure somewhere on the screen randomly
+  treasure.x = 32 + (Math.random() * (canvas.width - 64));
+  treasure.y = 32 + (Math.random() * (canvas.height - 64));
 };
 
 // Update game objects
 var update = function (modifier) {
-	if (38 in keysDown) { // Player holding up
-		hero.y -= hero.speed * modifier;
-	}
-	if (40 in keysDown) { // Player holding down
-		hero.y += hero.speed * modifier;
-	}
-	if (37 in keysDown) { // Player holding left
-		hero.x -= hero.speed * modifier;
-	}
-	if (39 in keysDown) { // Player holding right
-		hero.x += hero.speed * modifier;
-	}
+  if (38 in keysDown) hero.y -= hero.speed * modifier; // up
+  if (40 in keysDown) hero.y += hero.speed * modifier; // down
+  if (37 in keysDown) hero.x -= hero.speed * modifier; // left
+  if (39 in keysDown) hero.x += hero.speed * modifier; // right
 
-	// Are they touching?
-	if (
-		hero.x <= (monster.x + 32)
-		&& monster.x <= (hero.x + 32)
-		&& hero.y <= (monster.y + 32)
-		&& monster.y <= (hero.y + 32)
-	) {
-		++monstersCaught;
-		reset();
-	}
+  // Are they touching?
+  if (
+    hero.x <= (treasure.x + 32)
+    && treasure.x <= (hero.x + 32)
+    && hero.y <= (treasure.y + 32)
+    && treasure.y <= (hero.y + 32)
+  ) {
+    ++treasuresCaught;
+    reset();
+  }
 };
 
 // Draw everything
 var render = function () {
-	if (bgReady) {
-		ctx.drawImage(bgImage, 0, 0);
-	}
 
-	if (heroReady) {
-		ctx.drawImage(heroImage, hero.x, hero.y);
-	}
+  // images
+  if (bgReady)      ctx.drawImage(bgImage, 0, 0);
+  if (heroReady)    ctx.drawImage(heroImage, hero.x, hero.y);
+  if (treasureReady) ctx.drawImage(treasureImage, treasure.x, treasure.y);
 
-	if (monsterReady) {
-		ctx.drawImage(monsterImage, monster.x, monster.y);
-	}
-
-	// Score
-	ctx.fillStyle = "rgb(250, 250, 250)";
-	ctx.font = "24px Helvetica";
-	ctx.textAlign = "left";
-	ctx.textBaseline = "top";
-	ctx.fillText("Monsterrs caught: " + monstersCaught, 32, 32);
+  // score
+  ctx.fillStyle = "rgb(250, 250, 250)";
+  ctx.font = "24px Helvetica";
+  ctx.textAlign = "left";
+  ctx.textBaseline = "top";
+  ctx.fillText("Gold collected: " + treasuresCaught, 32, 32);
 };
 
 // The main game loop
 var main = function () {
-	var now = Date.now();
-	var delta = now - then;
+  var now = Date.now();
+  var delta = now - then;
 
-	update(delta / 1000);
-	render();
+  update(delta / 1000);
+  render();
 
-	then = now;
+  then = now;
 
-	// Request to do this again ASAP
-	requestAnimationFrame(main);
+  // Request to do this again ASAP
+  requestAnimationFrame(main);
 };
 
 // Cross-browser support for requestAnimationFrame
-var w = window;
-requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+var requestAnimationFrame =
+     window.requestAnimationFrame
+  || window.webkitRequestAnimationFrame
+  || window.msRequestAnimationFrame
+  || window.mozRequestAnimationFrame;
 
 // Let's play this game!
 var then = Date.now();
